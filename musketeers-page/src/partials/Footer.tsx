df3a7e4a6bedfaa21e94b26f4ff42661325f0c79
zainen <zainensuzuki@gmail.com>
2021-03-30 12:47:21 -0700

    footer

--- musketeers-page/src/partials/Footer.tsx
+++ musketeers-page/src/partials/Footer.tsx
@@ -1,7 +1,23 @@
 const Footer: React.FC = () => {
   return (
     <>
+      <div>
+        <div>
+          <h4>Find Us</h4>
+          <div>
+            <div>
+              <b>Email:</b>
+              <a href='mailto: info@musketeers.ca'>info@musketeers.ca</a>
+            </div>
+            <div>
+              <b>Hours</b>
+              <p>Monday: 5:30PM - 7:30PM</p>
+              <p>Saturday 12:00PM-2:00PM</p>
+            </div>
+          </div>
 
+        </div>
+      </div>
     </>
   )
 }
