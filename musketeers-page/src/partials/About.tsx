34def04357a6be344831a89c23d64615dd70e785
zainen <zainensuzuki@gmail.com>
2021-03-30 12:36:42 -0700

    home page

--- musketeers-page/src/partials/About.tsx
+++ musketeers-page/src/partials/About.tsx
@@ -3,7 +3,10 @@ import about_sport from '../images/about_sport.png'
 const About: React.FC = () => {
   return (
     <>
+    <div>
       <img src={about_sport} alt="about_sport" />
+      
+    </div>
     </>
   )
 }
