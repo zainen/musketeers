0800c953d2c31cf5836857789d109aa069ea0d69
zainen <zainensuzuki@gmail.com>
2021-04-13 15:48:23 -0700

    tab icon && title

--- musketeers-page/src/components/Footer.tsx
+++ musketeers-page/src/components/Footer.tsx
@@ -4,16 +4,16 @@ const Footer: React.FC = () => {
   return (
     <>
       <div className='footer'>
-        <h3>Find Us</h3>
+        <h3 className='foot-colour'>Find Us At:</h3>
         <div className='footer-info'>
           <div className='email-column'>
-            <b className='space-bottom'>Email: </b>
+            <b className='space-bottom'>Email </b>
             <a className='a-link' href='mailto: info@musketeers.ca'>info@musketeers.ca</a>
           </div>
           <div>
-            <b>Hours:</b>
+            <b>Bonsor Hours</b>
             <p>Monday: 5:30PM - 7:30PM</p>
-            <p>Saturday 12:00PM-2:00PM</p>
+            <p>Saturday: 12:00PM-2:00PM</p>
           </div>
         </div>
 
