da374d41f8d335a93454814d46b4551055447940
zainen <zainensuzuki@gmail.com>
2021-04-19 12:37:55 -0700

    fix footer smalls screen css

--- musketeers-page/src/components/Header.tsx
+++ musketeers-page/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import './header.css'
 import { BrowserRouter as  Route, Link } from 'react-router-dom'
+import tL from '../images/three-line.png'
 <Route />
 const Header: React.FC = () => {
   return (
@@ -11,7 +12,9 @@ const Header: React.FC = () => {
         <Link className='link' onClick={e => e.stopPropagation} to="/about-fencing">About Fencing</Link>
       </div>
       <div className="dropdown-nav">
-        <button className='dropbtn'>headers</button>
+        <button className='dropbtn'>
+          <img className='nav-icon' src={tL} alt="Nav"/>
+        </button>
           <div className="dropdown-content">
             <Link onClick={e => e.stopPropagation} to="/">Home</Link>
             <Link onClick={e => e.stopPropagation} to="/contact-us">Contact Us</Link>
